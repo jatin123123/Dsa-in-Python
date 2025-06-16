bce7ad577a10414b028562be0a7f4f1c80fdb358
Jatin Jangid <141016321+jatin123123@users.noreply.github.com>
2025-06-16 07:01:56 +0000

    starting sorting

---
+++ 08_SelectionSort.py
@@ -0,0 +1,9 @@
+nums = [12,34,23,5,23,53,23,5,23,23]
+lnth = len(nums)
+for i in range(lnth):
+    min = i
+    for j in range(i+1,lnth):
+        if nums[j] < nums[min]:
+            nums[min],nums[j] = nums[j],nums[min]
+
+print(nums)
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Dsa-in-Python
+# List of Questions
+
+1 -> 01_Extracting_digits
+2 -> 02_palindrome
+3 -> 03_armstrome
+4 -> 04_factors
+5 -> 05_dect_frequency
+6 -> 06_hashing
+7 -> disct
